0680ff27ea6d6e048bef96b933c616892896f7ac
Manuel Munoz <manuelmunozr.92@gmail.com>
2020-02-15 18:52:04 -0300

    Initial Commit

---
+++ Gallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gallery
+//
+//  Created by Manuel Munoz on 15/02/2020.
+//  Copyright © 2020 Manuel Munoz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
